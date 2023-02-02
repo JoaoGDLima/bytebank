27710a84119f76757b168f766780163358fa656a
JoaoGDLima <joaolima@cta.com.br>
2023-02-02 07:25:26 -0300

    Manipulating the variables

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    print("Bem vindo ao Bytebank")
+    println("Bem vindo ao Bytebank")
+
+    val titular = "João Guilherme"
+    val numeroConta = 1000
+    var saldo = 0.0
+
+    saldo = 100 + 2.0
+    saldo += 2.0
+
+    println("Titular: $titular")
+    println("Número da conta: $numeroConta")
+    println("saldo da conta: $saldo")
 }
