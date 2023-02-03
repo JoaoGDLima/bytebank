7270b84423925ced004169c50d4902d8e1eb6101
JoaoGDLima <joaolima@cta.com.br>
2023-02-03 10:38:58 -0300

    IImplemented transfer between bank account

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,31 +3,51 @@ fun main() {
 
     val conta1 = Conta()
     conta1.titular = "Alex"
-    conta1.depositar(100.0)
+    conta1.deposita(100.0)
 
     val conta2 = Conta()
     conta2.titular = "Fran"
 
     println("Saldo Alex: ${conta1.saldo}")
+
+    if (conta1.transfere(50.0, conta2)) {
+        println("Transferência realizada!")
+    } else {
+        println("Falha ao realizar transferência!")
+    }
+
+    println("Saldo Alex: ${conta1.saldo}")
+    println("Saldo Fran: ${conta2.saldo}")
 }
 
-class Conta{
+class Conta {
     var titular = ""
     var numeroConta = 0
     var saldo = 0.0
 
-    fun depositar(valor: Double){
+    fun deposita(valor: Double) {
         this.saldo += valor
     }
-    fun sacar(){
 
+    fun saca(valor: Double) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor
+        }
     }
-    fun transferir(){
 
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (this.saldo >= valor) {
+            this.saldo -= valor
+            destino.deposita(valor)
+
+            return true
+        }
+
+        return false
     }
 }
 
-fun testalacos(){
+fun testalacos() {
     for (i in 1..5) {
         val titular = "João Guilherme $i"
         val numeroConta = 1000 + i
